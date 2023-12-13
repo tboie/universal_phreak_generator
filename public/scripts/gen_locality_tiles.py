0e16059ae48a930d90051f5028fe2091f4fe4900
tboie <timboie@gmail.com>
2023-12-13 17:27:04 -0500

    refactor gen_locality_tiles.py

--- public/scripts/gen_locality_tiles.py
+++ public/scripts/gen_locality_tiles.py
@@ -1,10 +1,37 @@
-# creates a 2x2 inverse square tile for each 3x3 combination
-# creates layered inverse square diagram
+# creates 3x3 tiles of all possible combinations
+# creates layered inverse square tile using 3x3 tile
 
 import itertools
 import drawsvg as draw
 import numpy as np
 
+# tile size
+# note: check rectangle border when not a multiple of 2 and 3
+size = 480
+
+# total layers
+layers = 8
+
+# generate gray scale colors
+num_shades = layers + 1
+gray_values = np.linspace(0, 255, num_shades, dtype=int)
+colors = [f"#{val:02x}{val:02x}{val:02x}" for val in gray_values]
+
+# 3x3 base tile
+def genTile(m, path, color, colorBG):
+    d = draw.Drawing(size, size, origin=(0,0))
+    tSize = size / 3
+
+    for i in range(3):
+        for j in range(3):
+            if m[j][i] == 1:
+                d.append(draw.Rectangle(tSize * i, tSize * j, tSize, tSize, fill=color))
+            else:
+                d.append(draw.Rectangle(tSize * i, tSize * j, tSize, tSize, fill=colorBG))
+
+    d.save_svg(path)
+
+# split tuple to array
 def split(arr, size):
     arrs = []
     while len(arr) > size:
@@ -14,75 +41,45 @@ def split(arr, size):
     arrs.append(arr)
     return arrs
 
+
+# main...
+
 # all 3x3 combinations
 bits = [0, 1]
 combinations = list(itertools.product(bits, repeat=9))
 
-# create tile
 for idx, tuple in enumerate(combinations):
-    d = draw.Drawing(120, 120, origin=(0,0))
+    # 010111001 tuple to [[0, 1, 0], [1 ,1 ,1], [0, 0, 1]]
+    m = np.array(split(tuple, 3))
 
-    # ex) 010111001 = [[0, 1, 0], [1 ,1 ,1], [0, 0, 1]]
-    token = split(tuple, 3)
-    
-    m = np.array(token)
+    # flip array
     m = np.flip(m)
 
-    # 1
-    for i in range(3):
-        for j in range(3):
-            if m[j][i] == 1:
-                d.append(draw.Rectangle(20 * i, 20 * j, 20, 20, fill='black'))
-            else:
-                d.append(draw.Rectangle(20 * i, 20 * j, 20, 20, fill='white'))
-
-    # 2
-    for i in range(3):
-        for j in range(3):
-            if m[j][i] == 1:
-                d.append(draw.Rectangle(60 + (20 * i), 20 * j, 20, 20, fill='black'))
-            else:
-                d.append(draw.Rectangle(60 + (20 * i), 20 * j, 20, 20, fill='white'))
-
-    # 3
-    for i in range(3):
-        for j in range(3):
-            if m[j][i] == 1:
-                d.append(draw.Rectangle(20 * i, 60 + (20 * j), 20, 20, fill='black'))
-            else:
-                d.append(draw.Rectangle(20 * i, 60 + (20 * j), 20, 20, fill='white'))
-
-    # 4
-    for i in range(3):
-        for j in range(3):
-            if m[j][i] == 1:
-                d.append(draw.Rectangle(60 + (20 * i), 60 + (20 * j), 20, 20, fill='black'))
-            else:
-                d.append(draw.Rectangle(60 + (20 * i), 60 + (20 * j), 20, 20, fill='white'))
-
+    # file name and path
     file = ""
     for p in tuple:
         file = file + str(p)
     
-    path = "tiles/" + file + ".svg"
-    d.save_svg(path)
+    # edit?
+    path = "../tiles/" + file + ".svg"
     
-    # create layered inverse diagram using tile
-    d = draw.Drawing(120, 120, origin=(0,0))
-    
-    d.append(draw.Image(0, 0, 120, 120, path, embed=True, opacity=0.5))
+    # create base 1x1 tile
+    genTile(m, path, colors[1], colors[0])
 
-    d.append(draw.Image(0, 0, 60, 60, path, embed=True, opacity=0.5))
-    d.append(draw.Image(60, 0, 60, 60, path, embed=True, opacity=0.5))
-    d.append(draw.Image(0, 60, 60, 60, path, embed=True, opacity=0.5))
-    d.append(draw.Image(60, 60, 60, 60, path, embed=True, opacity=0.5))
+    # initialize svg
+    d = draw.Drawing(size, size, origin=(0,0))
 
-    for i in range(0,6):
-        d.append(draw.Image(0 + (i * 20), 0, 20, 20, path, embed=True, opacity=0.5))
-        d.append(draw.Image(0 + (i * 20), 20, 20, 20, path, embed=True, opacity=0.5))
-        d.append(draw.Image(0 + (i * 20), 40, 20, 20, path, embed=True, opacity=0.5))
-        d.append(draw.Image(0 + (i * 20), 60, 20, 20, path, embed=True, opacity=0.5))
-        d.append(draw.Image(0 + (i * 20), 80, 20, 20, path, embed=True, opacity=0.5))
-        d.append(draw.Image(0 + (i * 20), 100, 20, 20, path, embed=True, opacity=0.5))
+    # apply 1x1 tile
+    d.append(draw.Image(0, 0, size, size, path, embed=True, opacity=1))
 
-    d.save_svg('tiles/' + file + "_layers.svg")
+    # layers loop
+    for layer in range(2, layers + 1):
+        genTile(m, path, colors[layer], "transparent")
+        
+        cSize = size / layer
+        for x in range(0, layer):
+            for y in range(0, layer):
+                d.append(draw.Image(x * cSize, y * cSize, cSize, cSize, path, embed=True, opacity=1))
+
+    # save layers tile
+    d.save_svg('../tiles/' + file + "_layers.svg")
