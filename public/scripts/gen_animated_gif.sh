15d09f40917640729c4848139a0f89beb8ce81b2
tboie <timboie@gmail.com>
2024-03-23 17:03:09 -0400

    gen_animated_gif.sh framerate

--- public/scripts/gen_animated_gif.sh
+++ public/scripts/gen_animated_gif.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
  
 # recursively iterate directory
-# command: ./gen_animated_gif [dir]
+# command: ./gen_animated_gif [dir] [framerate]
 # output: [dir]/gen_animated.gif
-# todo: set gif speed
 
-find $1 -type d -exec ffmpeg -y -f image2 -i "{}/gen_%08d.png" "{}/gen_animated.gif" \;
+find $1 -type d -exec ffmpeg -y -framerate $2 -f image2 -i "{}/gen_%08d.png" "{}/gen_animated.gif" \;
